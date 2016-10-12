615ea5b9d7c1a44c431978acf809aeb59b7b79dc
VictorADS <victor.ads75@gmail.com>
2016-10-12 12:25:44 +0200

    Added new location update

--- app/src/main/java/com/veleasy/veleasy/MapsActivity.java
+++ app/src/main/java/com/veleasy/veleasy/MapsActivity.java
@@ -3,12 +3,20 @@ package com.veleasy.veleasy;
 import android.*;
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,17 +24,30 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
     public static final int MY_PERMISSIONS_REQUEST_ACCES_FINE_LOCATION=123;
 
     private GoogleMap mMap;
-
+    private GoogleApiClient mGoogleApiClient = null;
+    private Location mLastLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+            mGoogleApiClient.connect();
+            Log.e("Yo","Init");
+        }else{
+            Log.e("Yo","Init2");
+            mGoogleApiClient.connect();
+        }
         int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
         if(permissionCheck != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
@@ -34,6 +55,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }else {
            initMapAsync();
         }
+
+        new CountDownTimer(6000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                Log.e("Toz","AZEA");
+            }
+
+            public void onFinish() {
+                mLastLocation.setLatitude(mLastLocation.getLatitude()+0.001d);
+                mLastLocation.setLongitude(mLastLocation.getLongitude()+0.001d);
+                updatePos(new Location(mLastLocation));
+            }
+
+        }.start();
 
 
     }
@@ -55,13 +90,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
+    public void updatePos(Location l){
+        LatLng sydney;
+        if(mLastLocation!=null)
+            sydney = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        else
+            sydney = new LatLng(2,2);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("5"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.zoomIn());
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+
+    }
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
@@ -78,5 +120,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             }
         }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        Log.e("Lol","Je passe connecte");
+        updatePos(mLastLocation);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e("Lol","Je passe suspend");
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e("Lol","Je passe failed");
+
     }
 }
